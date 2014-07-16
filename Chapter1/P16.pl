e4e49b75cae96721b3f5d4f674d9bd14a4b80103
ktotoron <totoron.ipod.touch@gmail.com>
2014-07-16 14:39:15 +0000

    P016

---
+++ Chapter1/P16.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use utf8;
+use 5.018;
+use Module::CoreList;
+
+print $Module::CoreList::version{5.01400}{CPAN};
+
+__END__
+
---
+++ Chapter1/template.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use utf8
+use 5.018;
+
+
+__END__
+
